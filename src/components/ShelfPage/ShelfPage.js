a748ed31c2a3d3565369c9db30e7de0c16312568
Hmoovzong Vang <hmoovzongvang@gmail.com>
2024-02-01 14:43:01 -0600

    created a shelf saga and shelf reducer to display shelf items on DOM

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,22 +1,31 @@
 // Display all Items
-// The Shelf page (ShelfPage.jsat /shelf) should show all of the items stored in the database in a list or table. 
+// The Shelf page (ShelfPage.jsat /shelf) should show all of the items stored in the database in a list or table.
 // This list should only be viewable to logged in (authenticated) users.
 
-
-// The Shelf page (`ShelfPage.js` at `/shelf`) should allow a user to add a new item to the database (which should immediately appear in the list). 
+// The Shelf page (`ShelfPage.js` at `/shelf`) should allow a user to add a new item to the database (which should immediately appear in the list).
 // **This view should only be viewable to logged in (authenticated) users.**
 // > NOTE: Image url should be a full path to an existing image on the web. You should not attempt to implement image upload for this.
 
-
 // app.use('/api/shelf', shelfRouter);
 
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
 
 function ShelfPage() {
+  const shelf = useSelector((store) => store.shelf);
+
   return (
     <div className="container">
       <h2>Shelf</h2>
-      <p>All of the available items can be seen here.</p>
+      <div>
+        {shelf.map((item) => (
+          <div key={item.id}>
+            <h3>{item.description}</h3>
+            <img src={item.image_url} />
+            <button>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/redux/sagas/shelf.saga.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { put, takeLatest } from "redux-saga/effects";
+
+function* fetchShelf() {
+  try {
+    const response = yield axios.get("/api/shelf");
+    yield put({ type: "SET_SHELF", payload: response.data });
+  } catch (error) {
+    console.log("GET no good", error);
+  }
+}
+
+function* shelfSaga() {
+  yield takeLatest("FETCH_SHELF", fetchShelf);
+}
+
+export default shelfSaga;
