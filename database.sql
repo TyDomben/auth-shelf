88e2faf95510833aabc7b8850df05502ce54e6d1
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2024-02-01 13:57:52 -0600

    POST SHELF working

--- database.sql
+++ database.sql
@@ -1,4 +1,3 @@
-
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
@@ -15,3 +14,17 @@ CREATE TABLE "item" (
     "image_url" VARCHAR (2083),
     "user_id" INT REFERENCES "user"
 );
+
+INSERT INTO
+    "item" ("description", "image_url", "user_id")
+VALUES
+    (
+        'coffee can of marbles',
+        'https://images.squarespace-cdn.com/content/v1/5d998e025f94b14095e01ede/1588501023067-DCVMAY2GHQG8XNOJ2XUT/2020-05-MarbleJar.jpg',
+        1
+    );
+
+SELECT
+    *
+FROM
+    "item";
--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,43 +1,68 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
-const { rejectUnauthenticated } = require('../modules/authentication-middleware')
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
 
 /**
  * Get all of the items on the shelf
  */
-router.get('/', rejectUnauthenticated, (req, res) => {
+router.get("/", rejectUnauthenticated, (req, res) => {
   const queryText = `SELECT * FROM "item";`;
-  console.log(req.user)
-  
-  pool.query(queryText)
-  .then(result => {
-    res.send(result.rows)
-  })
-  .catch(error => {
-    console.log('Error in GET: ', error)
-    res.sendStatus(500)
-  })
+  console.log(req.user);
+
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log("Error in GET: ", error);
+      res.sendStatus(500);
+    });
 });
 
 /**
  * Add an item for the logged in user to the shelf
+ * post info - ensuring you are logged in -  - rerender to see results
+ *
  */
-router.post('/', (req, res) => {
+router.post("/", rejectUnauthenticated, (req, res) => {
+  // initialize query text - sql query
+  const queryText = `
+  INSERT INTO
+    "item" ("description", "image_url", "user_id")
+    VALUES ($1, $2, $3);
+  `;
+  const queryParams = [req.body.description, req.body.image_url, req.user.id];
+  console.log("POST SHELF");
+
+  pool
+    .query(queryText, queryParams)
+    .then((result) => {
+      res.sendStatus(201)
+    })
+    .catch((error) => {
+      console.log("Error in POST: ", error);
+      res.sendStatus(500);
+    });
+  // initialize query paramaters that equals request body x2 and req.user -id that we verify with login
+  // sql
   // endpoint functionality
 });
 
 /**
  * Delete an item if it's something the logged in user added
  */
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // endpoint functionality
 });
 
 /**
  * Update an item if it's something the logged in user added
  */
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // endpoint functionality
 });
 
@@ -45,14 +70,14 @@ router.put('/:id', (req, res) => {
  * Return all users along with the total number of items
  * they have added to the shelf
  */
-router.get('/count', (req, res) => {
+router.get("/count", (req, res) => {
   // endpoint functionality
 });
 
 /**
  * Return a specific item by id
  */
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // endpoint functionality
 });
 
